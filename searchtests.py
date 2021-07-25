61ff70a93df7262312e0eb4d20e177dcc96984fc
Ian <sebastianvaz123@hotmail.com>
2021-07-25 01:03:42 -0500

    Segundo programa de practica
    Signed-off-by: Ian <sebastianvaz123@hotmail.com>

--- searchtests.py
+++ searchtests.py
@@ -9,7 +9,7 @@ class HomePageTests(unittest.TestCase):
         options = webdriver.ChromeOptions()
         options.binary_location = '/usr/bin/brave-browser'
         self.driver = webdriver.Chrome(
-            executable_path='/usr/bin/chromedriver', chrome_options=options)
+            executable_path='/usr/bin/chromedriver', options=options)
         driver = self.driver
         driver.get('http://demo-store.seleniumacademy.com/')
         driver.maximize_window()
@@ -23,21 +23,22 @@ class HomePageTests(unittest.TestCase):
         search_field = self.driver.find_element_by_name("q")
 
     def test_search_text_field_class_name(self):
-        search_field=self.driver.find_element_by_class_name("input-text required-entry")
+        search_field = self.driver.find_element_by_class_name("input-text")
 
     def test_search_button_enabled(self):
-        search_field=self.driver.find_element_by_class_name("button")
+        search_field = self.driver.find_element_by_class_name("button")
 
     def test_count_of_promo_banner_images(self):
-        banner_list= self.driver.find_element_by_class_name("promos")
-        banners=banner_list.find_element_by_tag_name("img")
-        self.assertEqual(3,len(banners))
+        banner_list = self.driver.find_element_by_class_name("promos")
+        print(banner_list)
+        banners = banner_list.find_elements_by_tag_name("img")
+        self.assertEqual(3, len(banners))
+        print(banners)
 
-    def tearDown(self) -> None:
+    def tearDown(self):
         self.driver.close()
-        return super().tearDown()
 
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2, testRunner=HTMLTestRunner(
-        output='reportes', report_name='Hello_world'))
+    unittest.main(verbosity=3, testRunner=HTMLTestRunner(
+        output='reportes', report_name='Search__test'))
